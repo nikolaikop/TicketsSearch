c3afa099126181b7b5b600663d616e52d9acc688
Nikolai Pustovoit <nikolaikop@gmail.com>
2021-11-28 13:29:17 -0700

    tickets search version 2

--- src/test/java/ru/netology/manager/ManagerSearchTest.java
+++ src/test/java/ru/netology/manager/ManagerSearchTest.java
@@ -13,23 +13,50 @@ class ManagerSearchTest {
     RepositorySearch repository = new RepositorySearch();
     ManagerSearch search = new ManagerSearch(repository);
 
-    Search search1 = new Search(1, 4000, "LED", "KUF", 240);
-    Search search2 = new Search(2, 3000, "SVO", "LAX", 180);
-    Search search3 = new Search(3, 2000, "LED", "KHV", 120);
-    Search search4 = new Search(4, 1000, "LED", "SVO", 60);
+    Search search1 = new Search(1, 8000, "LED", "KUF", 420);
+    Search search2 = new Search(2, 4000, "SVO", "LAX", 360);
+    Search search3 = new Search(3, 2000, "LED", "KHV", 300);
+    Search search4 = new Search(4, 1000, "LED", "SVO", 240);
+    Search search5 = new Search(5, 500, "LED", "KHV", 180);
+    Search search6 = new Search(6, 250, "SVO", "LED", 120);
+    Search search7 = new Search(7, 125, "KUF", "SVO", 60);
 
     @BeforeEach
     public void setUp() {
-        search.add(search1);
         search.add(search2);
-        search.add(search3);
+        search.add(search1);
         search.add(search4);
+        search.add(search6);
+        search.add(search7);
+        search.add(search5);
+        search.add(search3);
     }
 
     @Test
-    void sortIfFlightIsFound() {
-        Search[] expected = {search3};
+    void sortIfFlightIsFoundFew() {
+        Search[] expected = {search5, search3};
         Search[] actual = search.findAll("LED", "KHV");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void sortIfFlightIstFoundOne() {
+        Search[] expected = {search6};
+        Search[] actual = search.findAll("SVO", "LED");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void sortIfFlightIsNotFound() {
+        Search[] expected = {};
+        Search[] actual = search.findAll("LED", "LAX");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void sortIfFlightIsEmpty() {
+        Search[] expected = {};
+        Search[] actual = search.findAll("", "");
         Assertions.assertArrayEquals(expected, actual);
     }
 
